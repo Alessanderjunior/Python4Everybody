a2b1b7550615ad2d14b4e9a59c3f7b46f667e8a1
alessander.junior <alessander.junior@opah.com.br>
2021-08-11 15:15:51 -0300

    Exercise 07 02 data structure in Python

--- Data Structures with Python/ex_06_01.py
+++ Data Structures with Python/ex_06_01.py
@@ -1,6 +1,4 @@
-data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
-pos = data.find('.')
-print(data[pos:pos+3])
+
 
 # text = "X-DSPAM-Confidence:    0.8475"
 # textmod = text[24:32]
